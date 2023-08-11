1b0854212edf3abd24e6a9be21bc580357948fae
Mohamed SBII <bestlol011247801281@gmail.com>
2023-08-11 07:52:51 -0700

    done;

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+	if(n<0)
+	printf("%d is negative",n);
+	else if(n>0)
+        printf("%d is positive",n);
+	else if(n==0)
+        printf("%d is zero",n);
+}
