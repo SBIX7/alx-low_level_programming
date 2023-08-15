1217f3280772f715de59615b042c870a8b73cd3f
Mohamed SBII <bestlol011247801281@gmail.com>
2023-08-15 14:13:22 -0700

    done

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,15 +1,4 @@
-#ifndef sui
-#define sui
-
-#include <stdio.h>
-void _putchar(char T[])
-{
-int i = 0;
-while (T[i] != '\0')
-	{
-		putchar(T[i]);
-		i++;
-	}
-putchar('\n');
-}
+#ifndef myheader
+#define myheader
+void _putchar(char T[]);
 #endif
