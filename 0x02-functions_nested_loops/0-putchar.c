c4d6c7e04f68d83a58884a91ce5df308e588a75f
Mohamed SBII <bestlol011247801281@gmail.com>
2023-08-15 11:48:06 -0700

    done

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+char T[] = "_putchar";
+int i = 0;
+while (T[i] != '\0');
+	{
+		putchar(T[i]);
+		i++;
+	}
+return (0);
+}
