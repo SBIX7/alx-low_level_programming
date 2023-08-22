43e034505a6b3817da017960e9fe98e0ee77aa70
Mohamed SBII <bestlol011247801281@gmail.com>
2023-08-22 09:19:11 -0700

    done

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,12 +7,12 @@
  */
 void rev_string(char *s)
 {
-int i, b = strlen(s)/2;
+int i, b = strlen(s) / 2;
 char a;
 for (i = 0; i <= b; i++)
 {
 	a = s[i];
-	s[i] = s[strlen(s) - i];
-	s[strlen(s) - i] = a;
+	s[i] = s[strlen(s) - 1 - i];
+	s[strlen(s) - 1 - i] = a;
 }
 }
