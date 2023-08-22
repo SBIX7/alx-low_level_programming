664e9bf8391a19643a107a7d5b91ce887172c593
Mohamed SBII <bestlol011247801281@gmail.com>
2023-08-22 09:59:46 -0700

    done

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -7,18 +7,10 @@
  */
 void puts2(char *str)
 {
-int i = 1, a = strlen(str);
-while (str[i+1] != '\0')
-{
-	str[i] = str[0 + i];
-	i++;
-	a--;
-}
-str[a+1] = '\0';
-i = 0;
+int i = 0;
 while (str[i] != '\0')
 {
-	_putchar(str[i]);
-	i++;
+	putchar(str[i]);
+	i += 2;
 }
 }
