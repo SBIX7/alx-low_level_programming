db8baeeb8874a98b542f6a7ed323e10b23fff889
Mohamed SBII <bestlol011247801281@gmail.com>
2023-08-25 08:32:36 -0700

    done

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,14 @@
+#include <string.h>
+#include "main.h"
+
+ /**
+  * _strcmp -  compares two strings
+  * @s1: Fisrt string
+  * @s2: Second string
+  *
+  * Return: The value.
+  */
+int _strcmp(char *s1, char *s2)
+{
+	return (strcmp(s1,s2));
+}
