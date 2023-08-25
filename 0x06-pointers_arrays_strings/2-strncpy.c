cdab1381c5d1fc14fbf9701a4e53ab0f80f353f0
Mohamed SBII <bestlol011247801281@gmail.com>
2023-08-25 07:52:53 -0700

    done

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,15 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * _strncpy - Copying function.
+ * @dest: Dest str.
+ * @src: Src str.
+ * @n: Num of src.
+ *
+ * Return: Dest str and n str.
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	return (strncpy(dest,scr,n));
+}
