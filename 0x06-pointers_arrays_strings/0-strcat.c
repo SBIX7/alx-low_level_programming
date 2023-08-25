75e39107cb6c7634eef2c253c7ae4e2cd473db4e
Mohamed SBII <bestlol011247801281@gmail.com>
2023-08-25 07:25:10 -0700

    done

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -5,9 +5,9 @@
  * _strcat-function
  * @dest: string2
  * @src: string1
- * return; str1+str2
+ * return: str1+str2
  */
 char *_strcat(char *dest, char *src)
 {
-	return (strcat(dest,src));
+return (strcat(dest,src));
 }
