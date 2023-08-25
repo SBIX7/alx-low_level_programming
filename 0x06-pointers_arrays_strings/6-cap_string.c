e356a5b266d510819e8a7861e78dc3915398aaf4
Mohamed SBII <bestlol011247801281@gmail.com>
2023-08-25 09:40:06 -0700

    done

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -2,19 +2,28 @@
 #include "main.h"
 
 /**
- * cap_string - Capitalizes words in a string.
+ * cap_string - Capitalizes all words in a string.
  * @n: Input string.
  *
  * Return: Capitalized string.
  */
 char *cap_string(char *n)
 {
-int i = 0;
-while (n[i] != '\0')
-{
-	if (n[i] == ',' || n[i] == ';' || n[i] == '.' || n[i] == '!' || n[i] == '?' || n[i] == '"' || n[i] == '(' || n[i] == ')' || n[i] == '{' || n[i] == '}' || n[i] == ' ' || n[i] == '\t' || n[i] == '\n')
-		n[i+1] = toupper(n[i+1]);
-	i++;
-}
-return (n);
+    int i = 0;
+
+    while (n[i] != '\0')
+    {
+        if (n[i] == ',' || n[i] == ';' || n[i] == '.' || n[i] == '!' ||
+            n[i] == '?' || n[i] == '"' || n[i] == '(' || n[i] == ')' ||
+            n[i] == '{' || n[i] == '}' || n[i] == ' ' || n[i] == '\t' || n[i] == '\n')
+        {
+            if (n[i + 1] >= 'a' && n[i + 1] <= 'z')
+            {
+                n[i + 1] = toupper(n[i + 1]);
+            }
+        }
+        i++;
+    }
+
+    return (n);
 }
