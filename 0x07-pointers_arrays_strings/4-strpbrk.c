af3709087f87ae853f28e06ea076dec65990a3e5
Mohamed SBII <bestlol011247801281@gmail.com>
2023-08-28 14:37:46 -0700

    done

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -2,11 +2,11 @@
 #include "main.h"
 
 /**
- * _strpbrk - Fuction that return the first occurence of one char from a string in the src string.
- * @s: The source.
- * @accept: The string that contain the characters that we search.
+ * _strpbrk - Finds the first occurrence of one character from a string in the source string.
+ * @s: The source string.
+ * @accept: The string containing the characters to search.
  *
- * Return: The byte founded.
+ * Return: A pointer to the byte found, or NULL if not found.
  */
 char *_strpbrk(char *s, char *accept)
 {
