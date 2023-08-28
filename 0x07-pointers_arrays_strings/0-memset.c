af5ba8d6dad87adb2e9c983a7a299f647ef3dcf5
Mohamed SBII <bestlol011247801281@gmail.com>
2023-08-28 09:52:16 -0700

    done

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,15 @@
+#include <string.h>
+#include "main.h"
+
+ /**
+  * _memset - Function that fills memory with a constant byte.
+  * @s: The Pointer to bloc.
+  * @b: The value.
+  * @n: Number of bytes.
+  *
+  * Return: A pointer to a bloc.
+  */
+char *_memset(char *s, char b, unsigned int n)
+{
+return (memset(s,b,n));
+}
