4041f954c8221bc87bef84aa67badb37736fd474
Mohamed SBII <bestlol011247801281@gmail.com>
2023-08-31 10:41:23 -0700

    done

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,12 +8,10 @@
  */
 void _puts_recursion(char *s)
 {
-int i = 0;
-if (s[i] == '\0')
+if (s == '\0')
 {
 	_putchar ('\n');
-	return;
 }
-_putchar (s[i]);
+_putchar (s);
 _puts_recursion(s + 1);
 }
