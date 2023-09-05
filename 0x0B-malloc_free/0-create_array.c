ad21dc26334cd8741388fcb0a615ba6cebc2e98c
Mohamed SBII <bestlol011247801281@gmail.com>
2023-09-05 16:29:27 -0700

    done

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,10 +14,10 @@ char *a;
 unsigned int i;
 if (size == 0)
 	return (NULL);
-a = malloc(sizeof(unsigned int));
+a = malloc(sizeof(char) * size);
 if (a == NULL)
 	return (NULL);
-for (i = 0; i < size - 1; i++)
+for (i = 0; i < size; i++)
 {
 	a[i] = c;
 }
