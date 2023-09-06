526f9aa2697a2051b84559f236434f5a955f0d5a
Mohamed SBII <bestlol011247801281@gmail.com>
2023-09-05 17:59:23 -0700

    done

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -14,9 +14,20 @@ int **arr, i, j;
 if (width <= 0 || height <= 0)
 	return (NULL);
 arr = (int **)malloc(sizeof(int *) * height);
+if (arr == NULL)
+	return (NULL);
 for (i = 0; i < width; i++)
 {
 	arr[i] = malloc(sizeof(int) * width);
+	if (arr[i] == NULL)
+        {
+	for (j = 0; j < i; j++)
+		{
+			free(arr[j]);
+		}
+		free(arr);
+		return (NULL);
+	}
 	for (j = 0; j < width; j++)
 	{
 		arr[i][j] = 0;
