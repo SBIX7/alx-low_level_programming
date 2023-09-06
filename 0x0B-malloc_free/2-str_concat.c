a39758e91f9e317aa3dd4e00d9ca31c452c6d322
Mohamed SBII <bestlol011247801281@gmail.com>
2023-09-05 17:13:40 -0700

    done

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,6 +12,10 @@
 char *str_concat(char *s1, char *s2)
 {
 char *arr;
+if (s1 == NULL)
+	s1 = "";
+if (s2 == NULL)
+	s2 = "";
 arr = malloc(strlen(s1) + strlen(s2) + 1);
 if (arr == NULL)
 	return (NULL);
