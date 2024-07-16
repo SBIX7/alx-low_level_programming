c9fed915f63d6af205246511632c5f9bb77de96d
Sim7amdat <bestlol011247801281@gmail.com>
2024-07-16 14:11:27 -0700

    done

---
+++ 0x09-static_libraries/main.c
@@ -0,0 +1,12 @@
+#include "main.h"  // Inclure le fichier d'en-tête contenant les prototypes des fonctions
+
+int main(void)
+{
+    char quote[] = "\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick";
+
+    // Utilisation de la fonction _puts pour afficher la citation
+    _puts(quote);
+
+    return (0);
+}
+
