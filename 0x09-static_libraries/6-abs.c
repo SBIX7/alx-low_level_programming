33fd5efbdb91dced85770c7cf4a42ce38f42a433
Sim7amdat <bestlol011247801281@gmail.com>
2024-07-16 13:23:34 -0700

    done

--- 0x09-static_libraries/6-abs.c
+++ 0x09-static_libraries/6-abs.c
@@ -1,15 +1,6 @@
 #include "main.h"
 
-/**
- * _abs - computes the absolute value of an integer
- * @n: the int to check
- * Return: the absolute value of int
- */
-int _abs(int n)
+main()
 {
-	if (n >= 0)
-	{
-		return (n);
-	}
-	return (-n);
+
 }
