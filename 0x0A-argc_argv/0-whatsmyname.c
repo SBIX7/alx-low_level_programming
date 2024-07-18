b2bd8de6c083c2f70adbe619ca6c2e0c7f6c0bfd
Sim7amdat <bestlol011247801281@gmail.com>
2024-07-18 10:32:07 -0700

    done

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
-/*
- * a program that print the name of the programme
- * return 1
+/**
+ * a programm that return the name of itslef
+ * @argc: the number of argument commited to the programm from command line
+ * @argv: a pointer the a string
+ * Return: the name of the programme
  */
 
 int main(int argc, char **argv)
 {
 	printf("%s\n", argv[0*argc]);
-	return 0;
+	return (0);
 }
