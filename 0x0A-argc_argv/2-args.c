4a93ae9fab60c120b64df42cc81cc3af49cb408b
Sim7amdat <bestlol011247801281@gmail.com>
2024-07-18 13:11:27 -0700

    done

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -13,8 +13,9 @@
 int main(int argc, char const **argv)
 {
 	int i;
-	for(i = 0; i < argc; i++){
-		printf("%s\n", argv[i]);
+	for(i = 0; i < argc; i++)
+	{
+	printf("%s\n", argv[i]);
 	}
 	return (0);
 }
