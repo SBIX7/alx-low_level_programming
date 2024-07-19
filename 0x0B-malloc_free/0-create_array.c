511f4d8e1a9d9b57f611feafe5e7a0f4eb7e744e
Sim7amdat <bestlol011247801281@gmail.com>
2024-07-19 10:08:46 -0700

    done

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,8 +11,11 @@
 char *create_array(unsigned int size, char c)
 {
 	unsigned int i;
-	char *p = malloc(size);
+	char *p;
 
+	if (size == 0)
+	return (NULL);
+	p = malloc(size);
 	for (i = 0; i < size; i++)
 	{
 		p[i] = c;
