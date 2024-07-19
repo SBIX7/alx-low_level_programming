2dc9529d2435f41a2422fe1b89ee86c643c1417f
Sim7amdat <bestlol011247801281@gmail.com>
2024-07-19 11:30:40 -0700

    done

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -14,16 +14,23 @@ int **alloc_grid(int width, int height)
 	
 	if (width <= 0 || height <= 0)
 	return (NULL);
-	p = malloc(width);
-	*p= malloc(height);
+	p = malloc(width * sizeof(int *));
 	if (p == NULL)
 	return (NULL);
 	for (i = 0; i < width; i++)
 	{
-		for (j = 0; j < height; j++)
+		p[i] = malloc(height * sizeof(int));
+		if (p[i] == NULL)
 		{
-			p[i][j] = 0;
+			for (j = 0; j < i; j++)
+			{
+				free(p[j]);
+			}
+		free(p);
+		return (NULL);
 		}
+		for (j = 0; j < height; j++)
+		p[i][j] = 0;
 	}
 	return (p);
 }
