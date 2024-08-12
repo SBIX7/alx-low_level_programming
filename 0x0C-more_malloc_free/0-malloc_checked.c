d6f443d2a7ec1a09e819efad08c6c72d4ea32f82
Sim7amdat <bestlol011247801281@gmail.com>
2024-08-12 07:20:37 -0700

    done

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -11,7 +11,7 @@ void *malloc_checked(unsigned int b)
 {
 	void *p;
 
-	p = (void *)malloc(b * sizeof(int));
+	p = (void *)malloc(b);
 	if (p ==  NULL)
 	exit(98);
 	return (p);
--- 0x0C-more_malloc_free/README.md
+++ 0x0C-more_malloc_free/README.md
@@ -1,1 +1,2 @@
-SALAm 
+hello hello
+hello   
