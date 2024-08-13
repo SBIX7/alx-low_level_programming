05e0d1879a5a0efd7ac6ddf8ac025a8c9c4df867
Sim7amdat <bestlol011247801281@gmail.com>
2024-08-13 07:44:03 -0700

    done

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - program that print the name of the file.
+ *
+ * Return: zero if the programm execution turn succes
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
