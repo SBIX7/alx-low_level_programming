205ed86b0edd99ff64b1122cbdbb901fe7159825
Sim7amdat <bestlol011247801281@gmail.com>
2024-08-14 09:39:23 -0700

    done

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,9 @@
 #ifndef FUNCTIONS_POINTERS_H
 #define FUNCTIONS_POINTERS_H
+#include <stdlib.h>
 
 int _putchar(char c);
+void print_name_as_is(char *name);
+void print_name_uppercase(char *name);
 void print_name(char *name, void (*f)(char *));
 #endif
