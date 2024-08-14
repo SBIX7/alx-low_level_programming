b37e101605067f19e0863940b57503ac65fb7d59
Sim7amdat <bestlol011247801281@gmail.com>
2024-08-14 15:48:30 -0700

    done

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -15,21 +15,17 @@ int main(int argc, char **argv)
 	if (argc != 4)
 	{
 		printf("Error\n");
-		exit (98);
+		exit(98);
 	}
-	if (argv[2][0] != '-' &&
-	argv[2][0] != '*' &&
-	argv[2][0] != '/' &&
-	argv[2][0] != '+' &&
-	argv[2][0]!= '%')
+	if (get_op_func(argv[2]) == NULL || argv[2][1] != '\0')
 	{
 		printf("Error\n");
-		exit (99);
+		exit(99);
 	}
 	if ((argv[2][0] == '%' || argv[2][0] == '/') && argv[3][0] == '0')
 	{
 		printf("Error\n");
-		exit (100);
+		exit(100);
 	}
 	printf("%d\n", get_op_func(argv[2])(atoi(argv[1]), atoi(argv[3])));
 	return (0);
