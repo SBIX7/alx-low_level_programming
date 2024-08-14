7d06e6317e3dce24167bbdedf58dc1eabd1e3755
Sim7amdat <bestlol011247801281@gmail.com>
2024-08-14 12:44:06 -0700

    done

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,34 @@
+#include "3-calc.h"
+#include <stddef.h>
+#include <string.h>
+
+/**
+ * get_op_func -function that select the right function.
+ * @s: the operator.
+ *
+ * Return: adress to the function.
+ */
+
+int (*get_op_func(char *s))(int, int)
+{
+op_t ops[] = {
+{"+", op_add},
+{"-", op_sub},
+{"*", op_mul},
+{"/", op_div},
+{"%", op_mod},
+{NULL, NULL}
+};
+	int i;
+
+	i = 0;
+	while (ops[i].op != NULL)
+	{
+		if (strcmp(ops[i].op, s) == 0)
+		{
+			return (ops[i].f);
+		}
+		i++;
+	}
+	return (NULL);
+}
---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,35 @@
+#include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - calcul.
+ * @argc: number of arguments.
+ * @argv: char.
+ *
+ * Return: one if succes.
+ */
+int main(int argc, char **argv)
+{
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit (98);
+	}
+	if (argv[2][0] != '-' &&
+	argv[2][0] != '*' &&
+	argv[2][0] != '/' &&
+	argv[2][0] != '+' &&
+	argv[2][0]!= '%')
+	{
+		printf("Error\n");
+		exit (99);
+	}
+	if ((argv[2][0] == '%' || argv[2][0] == '/') && argv[3][0] == '0')
+	{
+		printf("Error\n");
+		exit (100);
+	}
+	return (get_op_func(argv[2])(atoi(argv[1]), atoi(argv[3])));
+}
