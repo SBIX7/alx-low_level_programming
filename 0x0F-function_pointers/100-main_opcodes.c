076eb37c9a53061af1aad2f1a000d78df29780e3
Sim7amdat <bestlol011247801281@gmail.com>
2024-08-15 04:44:41 -0700

    done

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - print it own opcode.
+ * @argc: number of arguments.
+ * @argv: list of pointers to strings.
+ *
+ * Return: zero if succes.
+ */
+int main(int argc, char **argv)
+{
+	int len = atoi(argv[1]), i;
+	unsigned char *op = (unsigned char *)main;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	if (len < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	for (i = 0; i < len; i++)
+	{
+		printf("%02x", op[i]);
+		if (i < len - 1)
+		printf(" ");
+	}
+	printf("\n");
+	return (0);
+}
