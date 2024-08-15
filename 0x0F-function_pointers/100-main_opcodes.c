68a8deb36effd9d5bb91e7e96a927cba64f7e1ca
Sim7amdat <bestlol011247801281@gmail.com>
2024-08-15 05:26:47 -0700

    done

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -9,19 +9,21 @@
  */
 int main(int argc, char **argv)
 {
-	int len = atoi(argv[1]), i;
-	char *op = (char *)main;
+	int len, i;
+	char *op;
 
 	if (argc != 2)
 	{
 		printf("Error\n");
 		exit(1);
 	}
+	len = atoi(argv[1]);
 	if (len < 0)
 	{
 		printf("Error\n");
 		exit(2);
 	}
+	op = (char *)main;
 	for (i = 0; i < len; i++)
 	{
 		printf("%02hhx", op[i]);
