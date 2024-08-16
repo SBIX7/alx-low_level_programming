3e361b9ccc98c29bab6a1cbfaf0a7ff560b68aca
Sim7amdat <bestlol011247801281@gmail.com>
2024-08-16 05:38:58 -0700

    done

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,16 @@
+#include <stdarg.h>
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * print_all - function that print all.
+ * @format: format.
+ *
+ * Return: void.
+ */
+void print_all(const char * const format, ...)
+{
+	va_list po;
+
+}
