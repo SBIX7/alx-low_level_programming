e85eb8b9431547a79ba56d45f9d83b25a39af1c5
Sim7amdat <bestlol011247801281@gmail.com>
2024-08-21 08:19:07 -0700

    doen

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -10,13 +10,13 @@
 size_t print_listint(const listint_t *h)
 {
 	size_t count = 0;
-	
+
 	if (h == NULL)
 	return (0);
 	while (h != NULL)
 	{
 		printf("%d\n", h->n);
-		count ++;
+		count++;
 		h = h->next;
 	}
 	return (count);
