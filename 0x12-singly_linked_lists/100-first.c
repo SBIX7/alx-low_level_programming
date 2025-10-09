062cf2c6190cf4b84a453f1e3d3956bb24a9c828
ahlele ah lelas <machichoghlek@gmail.com>
2025-10-09 05:41:26 -0700

    done

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * first - fucntion that excute before main
+ * Return: void
+ */
+
+void first(void)
+{
+	printf("You're beat! and yet, you must allow,");
+	printf("\nI bore my house upon my back!\n");
+}
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -18,7 +18,8 @@ typedef struct list_s
 	unsigned int len;
 	struct list_s *next;
 } list_t;
-
+void first(void) __attribute__ ((constructor));
+void free_list(list_t *head);
 list_t *add_node(list_t **head, const char *str);
 int _putchar(char c);
 size_t print_list(const list_t *h);
